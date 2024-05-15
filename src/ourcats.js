40b3e43e8809085a6c8ce214f5ea64df3a33dd14
Daryymaa <mdarmoed@users.noreply.github.com>
2024-05-15 15:31:02 +0300

    “pages”

---
+++ src/ourcats.js
@@ -0,0 +1,111 @@
+import './App.css';
+import './catscss.css';
+import Header from './Header';
+import Footer from './Footer';
+
+
+function Galerka() {
+  return (
+    <>
+      <Header />
+      <main>
+      <Galerkap />
+
+      </main>
+      <Footer /> 
+    </>
+  );
+}
+
+function Galerkap() {
+  return (
+    <>
+<div className="container">
+<div className='content-cont'> 
+
+ <div className='kotyata' >
+<h1>Наши котики</h1>
+<div className="galleri">
+  <div className="kitten-card">
+    <img src="https://i.redd.it/25o6v1z2cd371.jpg" alt="Kitten 1" />
+    <h3>Нори</h3>
+    <p>Возраст: 2 месяца </p>
+    <p>Маленькая серо-зеленая кошечка с игривым характером, обожает играть со всеми игрушками.</p>
+  </div>
+  <div className="kitten-card">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeL6qypoImU9zMNl5RJUCtxDd--9wHQanroVTz9FWTUg&s" alt="Kitten 2" />
+    <h3>Педро</h3>
+    <p>Возраст: 1 месяц </p>
+    <p>Веселый и игривый котенок, который будет верным другом и компаньоном во всех ваших приключениях.</p>
+  </div>
+  <div className="kitten-card">
+    <img src="https://sun9-83.userapi.com/impg/yWjrwrwufAq1nyZ0Ln9CAEPv2eprf-Ww_IWqGw/fGuoOAEcO0s.jpg?size=735x727&quality=95&sign=d6c6e316fde5c923befa11dd4c3333b1&c_uniq_tag=k_F1p03KExW7zDAxpayZIzTGDP6s9yTjjbyLGsJVKwA&type=album" alt="Kitten 2" />
+    <h3>Мистер Вискерс</h3>
+    <p>Возраст: 1 год </p>
+    <p>Котик, любящий праздники и веселье. Совсем недавно справил свой 1 день рождения и очень рад этому.</p>
+  </div>
+  <div className="kitten-card">
+    <img src="https://i.pinimg.com/736x/8c/1d/16/8c1d160f588fb71696263c9036e64523.jpg" alt="Kitten 2" />
+    <h3>Мила</h3>
+    <p>Возраст: 4 месяца </p>
+    <p>Котенок с нежным окрасом и добрым сердцем, ищет дом, где будет любима и ухожена</p>
+  </div>
+  <div className="kitten-card">
+    <img src="https://i.pinimg.com/736x/9e/44/01/9e440158ad7bd4470f02890fcb386b20.jpg" alt="Kitten 2" />
+    <h3>Жаннет</h3>
+    <p>Возраст: 1,5 года </p>
+    <p>Красивая кошка, познавшая "радости" материнства и мечтающая о стерилизации</p>
+  </div>
+  <div className="kitten-card">
+    <img src="https://i.pinimg.com/736x/07/9f/f3/079ff36e332723af8ef02780709ea035.jpg" alt="Kitten 2" />
+    <h3>Дарвин</h3>
+    <p>Возраст: 0,5 месяцев </p>
+    <p>Сын Жаннет. Любит наггетсы.</p>
+  </div>
+  <div className="kitten-card">
+    <img src="https://i.pinimg.com/736x/a0/08/f7/a008f7bb01c1f4ae7d826f38c2718a6e.jpg" alt="Kitten 2" />
+    <h3>Лялька</h3>
+    <p>Возраст: 0,5 месяцев </p>
+    <p>Дочь Жаннет и сестра Дарвина. Любит спать, кушать и снова спать.</p>
+  </div>
+  <div className="kitten-card">
+    <img src="https://i.pinimg.com/736x/0f/57/57/0f5757a358fa9b9e73d2cb4537bf3e73--tiger.jpg" alt="Kitten 2" />
+    <h3>Пушок</h3>
+    <p>Возраст: 2 года </p>
+    <p>Отец котят Жаннет. Подозревает её в измене.</p>
+  </div>
+  <div className="kitten-card">
+    <img src="https://i.pinimg.com/736x/4f/f0/39/4ff039750003448f1a33aefc4e7cb4f1.jpg" alt="Kitten 2" />
+    <h3>Чичи</h3>
+    <p>Возраст: 2 месяца </p>
+    <p>Единственная, кто готов к лету.</p>
+  </div>
+  <div className="kitten-card">
+    <img src="https://otvet.imgsmail.ru/download/274816565_1ef44785f225689b884f5e185e219247_800.png" alt="Kitten 2" />
+    <h3>Рыжик</h3>   
+    <p>Возраст: 2 года </p>
+    <p>Отличный парень и просто хороший кот. (Настоящий отец котят Жаннет)</p>
+  </div>
+  <div className="kitten-card">
+    <img src="https://i.pinimg.com/736x/0f/d9/ae/0fd9ae3e52964a760f8e740794563597.jpg" alt="Kitten 2" />
+    <h3>Тони</h3>
+    <p>Возраст: 3 года </p>
+    <p>Гений, миллиардер, плейбой, филантроп(утверждает, что он настоящий Тони Старк).</p>
+  </div>
+  <div className="kitten-card">
+    <img src="https://i.pinimg.com/736x/37/38/27/37382741873ab6dc6313f4c2a537d76c.jpg" alt="Kitten 2" />
+    <h3>Тайсон</h3>
+    <p>Возраст: 1 день </p>
+    <p>Сильный, мускулистый и самодостаточный кот, обожающий игры с мячом и выезды на прогулки.</p>
+  </div>
+</div>
+</div>
+</div>
+
+</div>
+    </>
+  );
+}
+
+
+export default Galerka;
